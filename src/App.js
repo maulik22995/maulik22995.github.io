5f9dd6764f6d96a434d026266d267c09f9a0ffb2
Maulik <maulik22995@gmail.com>
2026-01-26 19:11:41 +0530

    Fix unused imports for build

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Main from "./containers/Main";
 import { ThemeProvider } from "styled-components";
@@ -6,7 +6,6 @@ import { themes } from "./theme";
 import { GlobalStyles } from "./global";
 import { CursorProvider } from "react-cursor-custom";
 import { settings } from "./portfolio";
-import ReactGA from "react-ga";
 
 function App() {
   // useEffect(() => {
